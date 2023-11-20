7b1c322b9a49c726e6a8e86511aa83f5d757fe10
Juho Mäkeläinen <juhowm@metropolia.fi>
2023-11-20 13:29:59 +0200

    2.1 tehty

---
+++ moduuli 2/tehtävä 2.1.js
@@ -0,0 +1,10 @@
+'use strict';
+const luvut = [];
+luvut[0] = prompt("Anna 1. luku");
+luvut[1] = prompt("Anna 2. luku");
+luvut[2] = prompt("Anna 3. luku");
+luvut[3] = prompt("Anna 4. luku");
+luvut[4] = prompt("Anna 5. luku");
+for (let i = 4; i >= 0; i--){
+  console.log(luvut[i]);
+}
